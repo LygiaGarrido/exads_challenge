c967f73b4285a38cdb8e48c395c6ddaf743a20a5
lygia <lygia.garrido@mindswap.academy>
2022-12-21 19:34:27 +0000

    add logic to movie component

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -1,14 +1,48 @@
-import { MovieContainer, MovieCover } from "./styles";
+import { useState } from "react";
+import { MovieContainer } from "./styles";
+import { MovieCover } from "./MovieCover";
+import { movieList } from "../../movieList";
+import { Question } from "../Question";
+import { Card } from "../Card";
 
-function Movie(props) {
+function Movie() {
+  const [question, setQuestion] = useState(1);
+  const [movies, setMovies] = useState(movieList);
+  const [imdbUrl, setImdbUrl] = useState("");
 
-    const {onClick} = props;
+  
+  const movieFinder = function (movieId) {
+    return movies.find((movie) => movie.id === movieId);
+  };
+
+  const handleClick = (movieId) => {
+    setQuestion(question + 1);
+   
+    const relatedMovies = movieFinder(movieId).relatedMovies;
+
+    setMovies(relatedMovies);
+
+    if (!relatedMovies) {
+      const movieUrl = movieFinder(movieId).url;
+      setImdbUrl(movieUrl);
+    }
+  };
+
+  const movieCovers = movies?.map((movie) => (
+    <MovieCover
+      key={movie.id}
+      movieId={movie.id}
+      imageUrl={movie.image}
+      onClick={()=> handleClick(movie.id)}
+    />
+  ));
+
   return (
-    <MovieContainer>
-      <MovieCover onClick={onClick}/>
-      <MovieCover onClick={onClick} />
-      <MovieCover onClick={onClick} />
-    </MovieContainer>
+    <>
+      {question <= 3 && <Question questionNumber={question} />}
+      {question <= 3 && <MovieContainer>{movieCovers}</MovieContainer>}
+      {question > 3 && <Card href={imdbUrl} />}
+    </>
   );
 }
 
