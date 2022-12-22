d2a5956eb3732f901ab1aff5f15fcf264252c632
lygia <lygia.garrido@mindswap.academy>
2022-12-22 10:31:36 +0000

    refactor - add movie and rendering  logic to app

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,58 @@
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
+
 import { Background } from "./components/Background";
 import { Logo } from "./components/Logo";
 import { Disclaimers } from "./components/Disclaimer";
-import { Movie } from "./components/Movie";
+import { MovieContainer , MovieCover} from "./components/Movie";
+import { movieList } from "./utils/movieList";
+import { Question } from "./components/Question";
+import { Card } from "./components/Card";
 
 const domContainer = document.getElementById("root");
 const root = createRoot(domContainer);
 
 function App() {
+
+
+  const [question, setQuestion] = useState(1);
+  const [movies, setMovies] = useState(movieList);
+  const [imdbUrl, setImdbUrl] = useState("");
+
+  
+  const movieFinder = function (movieId) {
+    return movies.find((movie) => movie.id === movieId);
+  };
+
+  const handleClick = (movieId) => {
+    setQuestion(question + 1);
+   
+    const relatedMovies = movieFinder(movieId).relatedMovies;
+
+    setMovies(relatedMovies);
+
+    if (!relatedMovies) {
+      const movieUrl = movieFinder(movieId).url;
+      setImdbUrl(movieUrl);
+    }
+  };
+
+  const movieCovers = movies?.map((movie) => (
+    <MovieCover
+      key={movie.id}
+      movieId={movie.id}
+      imageUrl={movie.image}
+      onClick={()=> handleClick(movie.id)}
+    />
+  ));
+
+
   return (
     <Background>
       <Logo text="Your Logo" />
-      <Movie />
+      {question <= 3 && <Question questionNumber={question} />}
+      {question <= 3 && <MovieContainer>{movieCovers}</MovieContainer>}
+      {question > 3 && <Card href={imdbUrl} />}
       <Disclaimers textPrivacy={"Privacy Policy"} textTerms={"Terms of Use"} />
     </Background>
   );
